9c74b0c22dd463f527c88a8707ab62fbb50a7bd8
KleberMoreira3010 <klb_moreira@hotmail.com>
2024-08-27 21:13:30 -0300

    commit

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,5 +1,14 @@
+from loguru import logger
+
+logger.add("meu_app.log")
+
 def soma(x, y):
+    logger.info(x)
+    logger.info(y)
+    logger.info(x+y)
+
+
     return x+y
 
 
-print(soma(2,3))
+print(soma(2,"3"))
